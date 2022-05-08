6f837ed81a36b24b07afe33fe0613de13e5e1818
Angelina <angelinarwhite@gmail.com>
2022-05-08 15:38:01 -0400

    commit tree 7f192cc12e229723d543a5e71dfa18d95af5bffc

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const oldArr = []
+  const newArr = []
+  //1. break down word into an array of each char
+  for (let i in word)
+  {
+    oldArr.push(word[i])
+  };
+
+  //2. reverse old array into a new array
+  for (let j = word.length; j >= 0; j--)
+  {
+    newArr.push(word[j])
+  };
+
+  var oldArrStr = oldArr.join("");
+  var newArrStr = newArr.join("");
+
+  console.log("newArr: " + oldArrStr)
+  console.log("oldArr: " + newArrStr)
+
+  //3. compare old array and new array
+  if (newArrStr == oldArrStr)
+  {
+    //4. if same, then true
+    return true
+  }
+  else if (newArrStr != oldArrStr)
+  {
+    //5. if different, then false
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  const oldArr=[]
+  const newArr =[]
+  for (let i in word)
+  {
+    oldArr.push(word[i])
+  }
+  for (let i=word.length, i>=0, i--)
+  {
+    newArr.push(word[i])
+  }
+  if (newArr == OldArr)
+    return true
+  else if (newArr != oldArr)
+    return false
+
 */
 
 /*
